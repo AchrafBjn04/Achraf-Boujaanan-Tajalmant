d000653c1025b999794e3bd9c5bc0085d942429d
Achraf Boujaanan <achrafboujaanan2004@gmail.com>
2024-09-12 03:14:38 +0200

    Seccion inicio responsive arreglada

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const port = 3000;
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.EMAIL_USER, // Tu correo
-        pass: process.env.EMAIL_PASS  // Contraseña del correo
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     }
 });
 
